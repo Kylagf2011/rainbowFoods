ed282c3b0687e5ab580a75572238a8874bb3c35e
Kylagf2011 <kylagf2011@gmail.com>
2019-07-17 15:15:48 -0700

    Wednesday, 3:15

--- rainbowFoods/ViewController.swift
+++ rainbowFoods/ViewController.swift
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func glutenHelpTap(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.beyondceliac.org/celiac-disease/non-celiac-gluten-sensitivity/?gclid=EAIaIQobChMIy9Ktr_e84wIVfh6tBh0vHAEwEAAYASAAEgJK1fD_BwE")! as URL, options: [:], completionHandler: nil)
 
+    }
+    
+    @IBAction func glutenHelpTapTwo(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://g.co/kgs/D1yg6e")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func lactoseHelpTap(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.healthline.com/symptom/lactose-intolerance")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func lactoseHelpTapTwo(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://g.co/kgs/tq4dyM")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
 }
 
